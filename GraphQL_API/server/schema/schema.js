b894ec7df0c516a3d781c0aade739c96ce6741b7
hayes28 <heather.hayes918@gmail.com>
2023-11-05 16:40:45 -0600

    Task 2 Add graphiql2 and tasks data to schema.

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -4,6 +4,12 @@ const {
     GraphQLString,
     GraphQLInt,
 } = require('graphql');
+const _ = require('lodash');
+
+const tasks = [
+    { id: '1', title: 'Create your first webpage', weight: 1, description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)'},
+    { id: '2', title: 'Structure your webpage', weight: 1, description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order'},
+];
 
 // Define the task type
 const TaskType = new GraphQLObjectType({
@@ -25,7 +31,8 @@ const RootQuery = new GraphQLObjectType({
                 id: { type: GraphQLString }
             },
             resolve: (parent, args) => {
-                // resolver function
+                // Using lodash to find the task with the id passed in the args
+                return _.find(tasks, { id: args.id });
             }
         }
     }
