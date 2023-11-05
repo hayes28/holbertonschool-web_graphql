16581d0b9a53cfb067480cfc5990c275defdbd3d
hayes28 <heather.hayes918@gmail.com>
2023-11-05 15:09:08 -0600

    Task 0 Add schema and enable GraphiQL

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -1,16 +1,19 @@
 const {
+    GraphQLSchema,
     GraphQLObjectType,
     GraphQLString,
     GraphQLInt,
 } = require('graphql');
 
+// Define the task type
 const TaskType = new GraphQLObjectType({
     name: 'Task',
     fields: {
         id: { type: GraphQLString },
         title: { type: GraphQLString },
+        weight: { type: GraphQLInt },
         description: { type: GraphQLString },
     }
 });
 
-module.exports = { TaskType };
+module.exports = TaskType;
